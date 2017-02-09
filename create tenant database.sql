5839a77f8ed3d317c1ce6a7977de118ad4f867f2
SAP HANA Academy <hanaacademy@sap.com>
2017-02-09 13:01:08 +0100

    Live5
    Live5

--- create tenant database.sql
+++ create tenant database.sql
@@ -2,5 +2,6 @@ hdbsql -n localhost -d SYSTEMDB -i 90 -u system
 create database SHA system user password Password1;
 alter database SHA add 'diserver';
 alter database SHA add 'scriptserver';
+grant modeling to XSA_DEV;
 call grant_activated_role('sap.hana.xs.ide.roles::Developer','XSA_DEV');
 q 
